c4125cbe7f7a64440d2a264c00e5d3986f515e04
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2023-12-30 18:31:17 +0000

    Create app receiving orin03-dev bus messages

---
+++ settings.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', case_sensitive=True)
+
+    SERVICE_BUS_CONN_STRING: str
+
+
+# Configuration singleton to be passed around the application
+# This validates and loads the environment variables set in the .env file.
+settings = Settings()
