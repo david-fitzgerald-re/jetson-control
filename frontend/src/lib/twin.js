0236b0a4cf010abc64dc467857aec423b4325721
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2024-01-01 22:27:29 +0000

    Add JSDoc type hint for Twin

--- frontend/src/lib/twin.js
+++ frontend/src/lib/twin.js
@@ -1,11 +1,26 @@
 import { writable } from "svelte/store";
 
+/**
+ * @typedef {Object} Twin
+ * @property {Object.<string, any>} reported - The reported properties
+ * @property {Object.<string, any>} desired - The desired properties
+ */
+
+
+/**
+ * @type {Twin}
+ */
 const initialTwinState = {
   // TODO - this would possibly use a schema of the twin?
+  desired: {
+    colour: "unknown"
+  },
   reported: {
     colour: "unknown"
   }
 }
+
+// { reported: { [key: string]: any; }; desired: { [key: string]: any; }; }
 
 let currentTwin = writable(initialTwinState)
 
